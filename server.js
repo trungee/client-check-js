ebcbc7d6695e32774a55ab9201dfe9f42f6546e0
Trungee Nguyen <trungee.nguyen@gmail.com>
2017-03-09 22:01:55 +0700

    return client_check.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const http = require('http');
-const net = require('net');
-const url = require('url');
+
 
 const readline = require('readline');
 const fs = require('fs');
 
-var rl = 
+var rl = readline.createInterface({
+    input: fs.createReadStream('./client_check.js'),
+    output: process.sdtout,
+    terminal: false
+});
+
+var buffer = fs.readFileSync('./client_check.js');
 
 var proxy = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World");
+    res.writeHead(200, {"Content-Type": "application/javascript"});
+    res.write(buffer);
+    res.end();
+     
 });
 
 proxy.listen(8000);
